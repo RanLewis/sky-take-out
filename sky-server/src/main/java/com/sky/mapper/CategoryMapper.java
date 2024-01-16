83afdb3a0fcaf4546b4612db667ca65f4d31920d
RanLewis <ranlewis@outlook.com>
2024-01-16 23:42:51 +0800

    分类管理代码链接数据库

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -16,7 +16,7 @@ public interface CategoryMapper {
      * 插入数据
      * @param category
      */
-    @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
+    @Insert("insert into sky_take_out.category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
             " VALUES" +
             " (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Category category);

343d07c9d97de8f71370337f67023ecc668bdc8e
RanLewis <ranlewis@outlook.com>
2024-01-17 17:13:29 +0800

    新增菜品业务开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,9 +2,11 @@ package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
 import com.sky.result.Result;
+import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "菜品相关接口")
 public class DishController {
 
+    @Autowired
+    public DishService dishService;
     /**
      * 新增菜品
      */
     @PostMapping
     @ApiOperation(value = "新增菜品")
     public Result addDish(@RequestBody DishDTO dishDTO) {
+        log.info("新增菜品:{}",dishDTO);
+        dishService.addDishWithFlavor(dishDTO);
         return Result.success();
     }
 }

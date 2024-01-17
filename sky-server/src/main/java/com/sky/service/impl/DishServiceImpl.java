7062c3f317ae2a6cb24d37640f76cc1682bcd082
RanLewis <ranlewis@outlook.com>
2024-01-17 17:33:23 +0800

    菜品分页查询业务开发

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,16 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,5 +44,17 @@ public class DishServiceImpl implements DishService {
         if (flavors != null && !flavors.isEmpty()) {
             dishFlavorMapper.insertBatch(flavors);
         }
+    }
+
+    /**
+     * 分页查询
+     * @param dishDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(DishPageQueryDTO dishDTO) {
+        PageHelper.startPage(dishDTO.getPage(), dishDTO.getPageSize());
+        Page<DishVO> page = dishMapper.pageQuery(dishDTO);
+        return new PageResult(page.getTotal(), page.getResult());
     }
 }

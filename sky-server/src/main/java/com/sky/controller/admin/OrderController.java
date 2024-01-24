2242efd7fb2e0f2f35135debd05b92a2825c4a86
RanLewis <ranlewis@outlook.com>
2024-01-24 20:20:22 +0800

    查询订单详情业务开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -5,11 +5,13 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,7 @@ public class OrderController {
 
     /**
      * 条件查询订单
+     *
      * @param ordersPageQueryDTO
      * @return
      */
@@ -40,6 +43,7 @@ public class OrderController {
 
     /**
      * 订单数量统计
+     *
      * @return
      */
     @GetMapping("/statistics")
@@ -47,5 +51,18 @@ public class OrderController {
     public Result<OrderStatisticsVO> statistics() {
         OrderStatisticsVO orderStatisticsVO = orderService.statistics();
         return Result.success(orderStatisticsVO);
+    }
+
+    /**
+     * 获取订单详情
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/details/{id}")
+    @ApiOperation(value = "获取订单详情")
+    public Result<OrderVO> getDetailOrder(@PathVariable Long id) {
+        OrderVO orderVO = orderService.getOrderById(id);
+        return Result.success(orderVO);
     }
 }
